4c80359a6f46a8b5b7a8e5a7b83799894bf41bfb
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-27 21:59:36 -0300

    add

--- src/components/Prioridades/index.jsx
+++ src/components/Prioridades/index.jsx
@@ -10,25 +10,25 @@ import './style.css';
                     <circle cx="10.5" cy="10.5" r="10.5" fill="#FF0000"/>
                   
                 </svg>
-                <h3>Muito urgente</h3>
+                <h2>Muito urgente</h2>
               </section>
               <section>
               <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
                 <circle cx="10.5" cy="10.5" r="10.5" fill="#FAFF00"/>
                 </svg>
-                <h3>Urgente</h3>
+                <h2>Urgente</h2>
               </section>
               <section>
               <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
                 <circle cx="10.5" cy="10.5" r="10.5" fill="#0038FF"/>
                 </svg>
-                <h3>Pouco urgente</h3>
+                <h2>Pouco urgente</h2>
               </section>
               <section>
               <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
             <circle cx="10.5" cy="10.5" r="10.5" fill="#00CC08"/>
             </svg>
-            <h3>Realizado</h3>
+            <h2>Realizado</h2>
               </section>
             </div>
         )
