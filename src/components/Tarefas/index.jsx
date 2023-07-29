7324f598632fab48df033e2ffae99bc82c4572df
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-29 06:09:31 -0300

    teste

--- src/components/Tarefas/index.jsx
+++ src/components/Tarefas/index.jsx
@@ -6,29 +6,34 @@ import { useState } from 'react';
     
     const [color, setColor] = useState('')
 
-console.log(prioridade);
-
-    if (prioridade == '0') {
-        setColor('#FF0000')
-    }
-    if (prioridade == '1') {
-        setColor('#FAFF00')
-    }
-    if (prioridade == '2') {
-        setColor('#0038FF')
-    }
-    if (prioridade == '3') {
-        setColor('#00CC08')
-    }
+    switch (prioridade) {
+        case "0":
+            setColor('#FF0000')
+            break;
+        case "1":
+            setColor('#FAFF00')
+            break;
+        case "2":
+            setColor('#0038FF')
+            break;
+        case "3":
+            setColor('#00CC08')
+            break;
     
+        default: 
+            break;
+    }
 
-    
+   
+
     return(
         <div className='tarefa'>
+            <section>
+
             <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
-                    <circle cx="10.5" cy="10.5" r="10.5" fill="#FF0000"/>
-                  
+                    <circle cx="10.5" cy="10.5" r="10.5" fill={color}/>
                 </svg>
+            </section>
                   <h2>{texto}</h2>
         </div>
     )
